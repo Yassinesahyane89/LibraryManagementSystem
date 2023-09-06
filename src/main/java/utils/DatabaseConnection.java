74fcc56eabe43457140e7b7acb03d49a3ec61b6d
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-09-06 10:25:02 +0100

    Implemented database connection functionality.

--- src/main/java/utils/DatabaseConnection.java
+++ src/main/java/utils/DatabaseConnection.java
@@ -1,4 +1,25 @@
 package utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DatabaseConnection {
+    // JDBC URL, username, and password of MySQL server
+    private static final String URL = "jdbc:mysql://localhost:3306/librarymanagementsystem";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    // JDBC variables for managing the connection
+    private static Connection connection;
+    public static void main(String[] args) {
+
+        if (connection == null) {
+            try {
+                // Create a connection to the database
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connected to the database");
+            } catch (SQLException e) {
+                System.err.println("Error connecting to the database: " + e.getMessage());
+            }
+        }
+    }
 }
