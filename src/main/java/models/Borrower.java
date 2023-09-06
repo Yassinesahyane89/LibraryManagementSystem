d735f0091274b18359a5b5af61c3408ea3d9567f
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2023-09-06 10:02:55 +0100

    Added attributes, getters, setters, and constructor for the Borrow class.

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,4 +1,38 @@
 package models;
 
+import java.util.List;
+
 public class Borrower {
+    private String memberNumber;
+    private String name;
+    private List<LoanRecord> loanRecords;
+
+    public Borrower(String memberNumber, String name) {
+        this.memberNumber = memberNumber;
+        this.name = name;
+    }
+
+    public String getMemberNumber() {
+        return memberNumber;
+    }
+
+    public void setMemberNumber(String memberNumber) {
+        this.memberNumber = memberNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Borrower{" +
+                "memberNumber='" + memberNumber + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
